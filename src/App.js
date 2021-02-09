155e2ec9e95030ff8bc7736a084367e5bd587997
kali-user <hanesmitter3@gmail.com>
2021-02-09 09:33:48 +0300

    development

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import alanBtn from '@alan-ai/alan-sdk-web';
 import NewsCards from './components/NewsCards/NewsCards';
 
 const App = () => {
-    const [newsArticle, setNewsArticle] = useState();
+    const [newsArticle, setNewsArticle] = useState([]);
     const alanSDKKey = '7e5e70d2d75af1ee5a6f4410100b8bcf2e956eca572e1d8b807a3e2338fdd0dc/stage';
     useEffect(() => {
         alanBtn({
@@ -20,7 +20,7 @@ const App = () => {
     return (
         <div>
             <h1>Alan Ai News Application</h1>
-            <NewsCards article = {newsArticle}/>
+            <NewsCards article={newsArticle}/>
         </div>
     );
 }
--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { Grid, Typography, Grow } from '@material-ui/core';
 
 import NewsCard from '../NewsCard/NewsCard.js';
 
-const NewsCards = ( {articles} ) => {
+const NewsCards = ( {article : articles} ) => {
     return (
-        <div>
-            <h1>NewsCards</h1>
-            {articles.map((article, i) => (
-                <NewsCard />
-            ))}
-        </div>
+        <Grow in>
+            <Grid container alignItems='stretch'>
+                {articles.map((article, i) => (
+                    <Grid item xs={12} sm={6} md={4} lg={3} style={{display: 'flex'}}>
+                        <NewsCard article={article} i={i}/>
+                    </Grid>
+                ))}
+            </Grid>
+        </Grow>
     );
 }
 
